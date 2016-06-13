edfd462c320d41a027cc94bd3501babd18610f23
Zachary Cox <zcox2@illinois.edu>
2016-06-12 23:53:15 -0500

    Tap recognition now properly moves origin and redraws axes and line. Currently uses sloppy origin and newOrigin with boolean originHasChanged. Needs to be refactored.

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -47,27 +47,65 @@ class GraphView: UIView {
         
     }
     
-    func generateLineFromStoredOperation() -> UIBezierPath {
-        let maxX = bounds.width / 2 / pointsPerUnit // maxX is in graph units
-        var x = -Double(maxX)
-        var y = operation(x)
-        
-        let line = UIBezierPath()
-        line.moveToPoint(pointInGraphView(CGPoint(x: x, y: y)))
-        
-        while(x < Double(maxX)) {
-            x += 1 / Double(pointsPerUnit)
-            y = operation(x)
-            line.addLineToPoint(pointInGraphView(CGPoint(x: x, y: y)))
+    private func pointInGraphUnits(point: CGPoint) -> CGPoint {
+        if originHasChanged {
+            return CGPoint(
+                x: (point.x - newOrigin.x) / pointsPerUnit,
+                y: (point.y - newOrigin.y) / pointsPerUnit
+            )
+        } else {
+            return CGPoint(
+                x: origin.x + point.x * pointsPerUnit,
+                y: origin.y - point.y * pointsPerUnit
+            )
         }
-        return line
+    }
+    
+    func generateLineFromStoredOperation() -> UIBezierPath {
+        if originHasChanged {
+            let minX = -newOrigin.x / pointsPerUnit // maxX is in graph units
+            let maxX = (bounds.width - newOrigin.x) / pointsPerUnit
+            var x = Double(minX)
+            var y = operation(x)
+            
+            let line = UIBezierPath()
+            line.moveToPoint(pointInGraphView(CGPoint(x: x, y: y)))
+            
+            while(x < Double(maxX)) {
+                x += 1 / Double(pointsPerUnit)
+                y = operation(x)
+                line.addLineToPoint(pointInGraphView(CGPoint(x: x, y: y)))
+            }
+            return line
+        } else {
+            let minX = -origin.x / pointsPerUnit // maxX is in graph units
+            let maxX = (bounds.width - origin.x) / pointsPerUnit
+            var x = Double(minX)
+            var y = operation(x)
+            
+            let line = UIBezierPath()
+            line.moveToPoint(pointInGraphView(CGPoint(x: x, y: y)))
+            
+            while(x < Double(maxX)) {
+                x += 1 / Double(pointsPerUnit)
+                y = operation(x)
+                line.addLineToPoint(pointInGraphView(CGPoint(x: x, y: y)))
+            }
+            return line
+        }
+        
     }
     
     let axisDrawer = AxesDrawer()
     
     override func drawRect(rect: CGRect) {
-        axisDrawer.contentScaleFactor = self.contentScaleFactor
-        axisDrawer.drawAxesInRect(bounds, origin: origin, pointsPerUnit: pointsPerUnit)
+        if originHasChanged {
+            axisDrawer.contentScaleFactor = self.contentScaleFactor
+            axisDrawer.drawAxesInRect(bounds, origin: newOrigin, pointsPerUnit: pointsPerUnit)
+        } else {
+            axisDrawer.contentScaleFactor = self.contentScaleFactor
+            axisDrawer.drawAxesInRect(bounds, origin: origin, pointsPerUnit: pointsPerUnit)
+        }
         
         let line = generateLineFromStoredOperation()
         
